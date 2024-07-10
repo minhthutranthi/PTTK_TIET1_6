655a27e5ab9bf8e448a0211cf5cf923b614ab140
thu <thuminhtran147002@gmail.com>
2024-07-10 10:57:18 +0700

    add ConsoleApplication1

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            RunProgram();
+        }
+
+        static void RunProgram()
+        {
+            int[] a = { 1, 2, 3, 4, 5 }; 
+            int sum = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+            }
+
+            Console.WriteLine("Tong cac : " + sum);
+        }
+    }
+}
